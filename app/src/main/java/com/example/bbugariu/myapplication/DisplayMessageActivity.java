e1b14053e8aac2e36ff1e33e5397dc5788551ea2
bbugariu <bogdan.bugariu@kampn.com>
2017-09-08 15:06:36 +0300

    initial commit

---
+++ app/src/main/java/com/example/bbugariu/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.example.bbugariu.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout TextView and send the string as its texts
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
